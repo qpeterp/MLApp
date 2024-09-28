8f4f662dbe2ce7be4a5aa52b7dff9a8e1dd23a7c
qpeterp <lsepeter0827@naver.com>
2024-09-28 10:47:04 +0900

    Refactor :: ImageAnalyzer targetSquatUpPose add targetShape

--- app/src/main/java/com/qpeterp/mlapp/viewmodel/action/ImageAnalyzer.kt
+++ app/src/main/java/com/qpeterp/mlapp/viewmodel/action/ImageAnalyzer.kt
@@ -12,8 +12,6 @@ import com.google.mlkit.vision.pose.defaults.PoseDetectorOptions
 import com.qpeterp.mlapp.data.action.TargetPose
 import com.qpeterp.mlapp.data.action.TargetShape
 import com.qpeterp.mlapp.utils.log
-import kotlin.math.abs
-import kotlin.math.atan2
 
 class ImageAnalyzer(actionViewModel: ActionViewModel) : ImageAnalysis.Analyzer {
     // 영상 포즈 감지
@@ -29,6 +27,12 @@ class ImageAnalyzer(actionViewModel: ActionViewModel) : ImageAnalysis.Analyzer {
             TargetShape(
                 PoseLandmark.RIGHT_ANKLE, PoseLandmark.RIGHT_KNEE, PoseLandmark.RIGHT_HIP, 90.0
             ),
+            TargetShape(
+                PoseLandmark.RIGHT_KNEE, PoseLandmark.RIGHT_HIP, PoseLandmark.RIGHT_SHOULDER, 90.0
+            ),
+            TargetShape(
+                PoseLandmark.LEFT_KNEE, PoseLandmark.LEFT_HIP, PoseLandmark.LEFT_SHOULDER, 90.0
+            ),
         )
     )
     private val targetSquatUpPose: TargetPose = TargetPose(
@@ -38,6 +42,12 @@ class ImageAnalyzer(actionViewModel: ActionViewModel) : ImageAnalysis.Analyzer {
             ),
             TargetShape(
                 PoseLandmark.RIGHT_ANKLE, PoseLandmark.RIGHT_KNEE, PoseLandmark.RIGHT_HIP, 180.0
+            ),
+            TargetShape(
+                PoseLandmark.RIGHT_KNEE, PoseLandmark.RIGHT_HIP, PoseLandmark.RIGHT_SHOULDER, 180.0
+            ),
+            TargetShape(
+                PoseLandmark.LEFT_KNEE, PoseLandmark.LEFT_HIP, PoseLandmark.LEFT_SHOULDER, 180.0
             ),
         )
     )
@@ -63,10 +73,6 @@ class ImageAnalyzer(actionViewModel: ActionViewModel) : ImageAnalysis.Analyzer {
         }
     }
 
-//    companion object {
-//        private const val offset = 15.0
-//    }
-
     @OptIn(ExperimentalGetImage::class)
     override fun analyze(imageProxy: ImageProxy) {
         val mediaImage = imageProxy.image ?: return
@@ -79,37 +85,6 @@ class ImageAnalyzer(actionViewModel: ActionViewModel) : ImageAnalysis.Analyzer {
                 // 분석이 성공적으로 완료되면 결과 처리
                 log("image result is :\n\n ${pose.allPoseLandmarks}")
                 onPoseDetected(pose)
-
-//                targetPose.targets.forEach { target ->
-//                    val (firstLandmark, middleLandmark, lastLandmark) = extractLandmark(
-//                        pose,
-//                        target
-//                    )
-//
-//                    if (landmarkNotFound(firstLandmark, middleLandmark, lastLandmark)) return@addOnSuccessListener
-//
-//                    val angle = calculateAngle(firstLandmark!!, middleLandmark!!, lastLandmark!!) // landmarkNotFound remove null
-//                    targetPose.targets.forEach { target ->
-//                        if (abs(angle - target.angle) > offset) {
-//                            return@addOnSuccessListener
-//                        }
-//                    }
-//
-//                    // 인식이 제대로 되면 실행되는 곳
-//
-//                    private val onPoseDetected: (pose: Pose) -> Unit = { pose ->
-//                        val isOSign = poseMatcher.match(pose, targetPoseOSign)
-//                        val isXSign = poseMatcher.match(pose, targetPoseXSign)
-//                        when {
-//                            isOSign -> {
-//                                //detect O Sign
-//                            }
-//                            isXSign -> {
-//                                //detect X Sign
-//                            }
-//                        }
-//                    }
-//                }
             }
             .addOnFailureListener { e ->
                 // 분석이 실패했을 때 예외 처리
@@ -119,45 +94,5 @@ class ImageAnalyzer(actionViewModel: ActionViewModel) : ImageAnalysis.Analyzer {
                 // 이미지 분석이 완료되었음을 CameraX에 알림 (이거 필수)
                 imageProxy.close()
             }
-    }
-
-    private fun calculateAngle(
-        firstLandmark: PoseLandmark,
-        middleLandmark: PoseLandmark,
-        lastLandmark: PoseLandmark
-    ): Double {
-        val angle = Math.toDegrees(
-            (atan2(
-                lastLandmark.position.y - middleLandmark.position.y,
-                lastLandmark.position.x - middleLandmark.position.x
-            ) - atan2(
-                firstLandmark.position.y - middleLandmark.position.y,
-                firstLandmark.position.x - middleLandmark.position.x
-            )).toDouble()
-        )
-        val absoluteAngle = abs(angle)
-//        if (absoluteAngle > 180) {
-//            absoluteAngle = 360 - absoluteAngle
-//        }
-        return absoluteAngle
-    }
-
-    private fun landmarkNotFound(firstLandmark: PoseLandmark?, middleLandmark: PoseLandmark?, lastLandmark: PoseLandmark?): Boolean {
-        return if (firstLandmark == null || middleLandmark == null || lastLandmark == null) true else false
-    }
-
-    private fun extractLandmark(
-        pose: Pose,
-        target: TargetShape
-    ): Triple<PoseLandmark?, PoseLandmark?, PoseLandmark?> {
-        return Triple(
-            extractLandmarkFromType(pose, target.firstLandmarkType),
-            extractLandmarkFromType(pose, target.middleLandmarkType),
-            extractLandmarkFromType(pose, target.lastLandmarkType)
-        )
-    }
-
-    private fun extractLandmarkFromType(pose: Pose, landmarkType: Int): PoseLandmark? {
-        return pose.getPoseLandmark(landmarkType)
     }
 }
