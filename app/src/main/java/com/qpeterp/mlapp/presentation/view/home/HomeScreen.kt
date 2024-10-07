cef45bc5fddfc37736a78bd70151decaa4013c35
qpeterp <lsepeter0827@naver.com>
2024-10-07 14:51:11 +0900

    Refactor :: Custom card composable separation [ 카드 뷰 빼서 관리한다는 뜻 ]

--- app/src/main/java/com/qpeterp/mlapp/presentation/view/home/HomeScreen.kt
+++ app/src/main/java/com/qpeterp/mlapp/presentation/view/home/HomeScreen.kt
@@ -1,4 +1,4 @@
-package com.qpeterp.mlapp.ui.home
+package com.qpeterp.mlapp.presentation.view.home
 
 import android.view.ViewGroup
 import android.webkit.WebView
@@ -7,10 +7,7 @@ import androidx.compose.animation.expandIn
 import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
 import androidx.compose.animation.shrinkVertically
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -19,18 +16,19 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.viewinterop.AndroidView
-import com.qpeterp.mlapp.data.home.HomeCardData
-import com.qpeterp.mlapp.ui.theme.MLAppTheme
+import com.qpeterp.mlapp.domain.model.home.HomeCardData
+import com.qpeterp.mlapp.presentation.view.common.Card
+import com.qpeterp.mlapp.presentation.view.theme.MLAppTheme
 
 @Composable
 fun HomeScreen() {
@@ -76,7 +74,8 @@ fun HomeCard(
         ) {
             Row(
                 modifier = modifier.fillMaxWidth(),
-                horizontalArrangement = Arrangement.SpaceEvenly
+                horizontalArrangement = Arrangement.SpaceEvenly,
+                verticalAlignment = Alignment.CenterVertically
             ) {
                 Text(
                     text = if (informationState.value) "!" else "?",
@@ -115,28 +114,8 @@ fun WebView(
             this.layoutParams = ViewGroup.LayoutParams(
                 ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT
             )
-            this.webViewClient = CustomWebViewClient()
         }
     }, update = {
         it.loadUrl(url)
     })
-}
-
-@Composable
-fun Card(
-    backgroundColor: Color,
-    horizontalPadding: Int,
-    modifier: Modifier = Modifier,
-    onClick: () -> Unit,
-    content: @Composable () -> Unit,
-) {
-    Box(modifier = modifier
-        .fillMaxWidth()
-        .padding(horizontal = horizontalPadding.dp)
-        .background(
-            color = backgroundColor, shape = RoundedCornerShape(16.dp)
-        )
-        .clickable { onClick() }) {
-        content()
-    }
 }
