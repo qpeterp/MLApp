3c9fb4cf393c67715dafbe4dabcc5a00f08e812e
qpeterp <lsepeter0827@naver.com>
2024-10-07 14:52:39 +0900

    Feat :: Check phone inclination [ 핸드폰 기울기 파악 ]

---
+++ app/src/main/java/com/qpeterp/mlapp/domain/usecase/action/PhoneOrientationDetector.kt
@@ -0,0 +1,43 @@
+package com.qpeterp.mlapp.domain.usecase.action
+
+import android.content.Context
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
+import com.qpeterp.mlapp.utils.log
+import kotlin.math.abs
+
+class PhoneOrientationDetector(context: Context) : SensorEventListener {
+    private val sensorManager: SensorManager =
+        context.getSystemService(Context.SENSOR_SERVICE) as SensorManager
+    private val accelerometer: Sensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)!!
+
+    var verticalHilt = true
+
+    init {
+        // 센서 리스너 등록
+        sensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_NORMAL)
+    }
+
+    override fun onSensorChanged(event: SensorEvent?) {
+        if (event != null && event.sensor.type == Sensor.TYPE_ACCELEROMETER) {
+            val y = event.values[1] // Y축 가속도 값
+            val z = event.values[2] // Z축 가속도 값
+
+            // Z축의 값이 9.8에 가까우면, 기기가 바닥에 수직으로 누워 있는 상태
+            if (Math.abs(z) < 1 && abs(y) > 9) {
+                verticalHilt = true
+                log("핸드폰이 세워져 있음")
+            }
+        }
+    }
+
+    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {}
+
+    fun unregister() {
+        // 센서 리스너 해제
+        log("센서 리스너 해제!")
+        sensorManager.unregisterListener(this)
+    }
+}
